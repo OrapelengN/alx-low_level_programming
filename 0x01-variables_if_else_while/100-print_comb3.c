e085d38211bd6b86143358745cda7b96e9bfeb63
OrapelengN <portiaoran@gmail.com>
2024-06-21 06:40:23 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,8 @@
 #include <time.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 
@@ -13,18 +14,18 @@ int d, p;
 
 for (d = '0'; d < '9'; d++)
 {
-	for (p = d + 1; p <= '9'; p++)
-	{
-		if (p != d)
-		{
-			putchar(d);
-			putchar(p);
-			if (d == '8' && p == '9')
-				continue;
-			putchar(',');
-			putchar(' ');
-		}
-	}
+for (p = d + 1; p <= '9'; p++)
+{
+if (p != d)
+{
+putchar(d);
+putchar(p);
+if (d == '8' && p == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
 }
 putchar('\n');
 return (0);
