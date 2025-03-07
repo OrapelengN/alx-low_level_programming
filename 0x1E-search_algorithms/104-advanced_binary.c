8d92be335745311f2c676d5bcd9b52c32456f534
OrapelengN <portiaoran@gmail.com>
2025-03-07 06:10:55 -0800

    11. Advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -31,16 +31,18 @@ int advanced_binary_recursive(int *array, size_t low, size_t high, int value)
 
 	if (array[mid] >= value)
 	{
-		if (array[mid] == value) {
-			if (mid == low || array[mid - 1] != value) {
+		if (array[mid] == value)
+		{
+			if (mid == low || array[mid - 1] != value)
+			{
 				return (mid);
 			}
 		}
 		return (advanced_binary_recursive(array, low, mid, value));
-	} else {
-		return (advanced_binary_recursive(
-					array, mid + 1, high, value));
 	}
+	else
+		return (advanced_binary_recursive(array,
+					mid + 1, high, value));
 }
 
 /**
