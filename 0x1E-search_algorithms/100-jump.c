79e58046c51891a73577735bc9c73a5253b9429a
OrapelengN <portiaoran@gmail.com>
2025-03-07 04:33:46 -0800

    7. Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * jump_search - searches for a value in a sorted array using Jump Search.
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: first index where value is located, or -1 if value is not found
+ */
 
 int jump_search(int *array, size_t size, int value)
 {
