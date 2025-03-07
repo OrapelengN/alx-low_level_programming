87fa398c453dc2fb6c37bcf98f32c4d98d3ad553
OrapelengN <portiaoran@gmail.com>
2025-03-07 11:01:36 -0800

    12. Jump search in a singly linked list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -15,35 +15,39 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t step, prev_index;
-	listint_t *curr, *prev;
+        size_t step, prev_index, jump_index;
+        listint_t *curr, *prev;
 
-	if (!list || size == 0)
-		return (NULL);
+        if (list == NULL || size == 0)
+                return (NULL);
 
-	step = sqrt(size);
-	prev = NULL;
-	curr = list;
+        step = sqrt(size);
+        prev = NULL;
+        curr = list;
+        prev_index = 0;
 
-	while (curr->next && curr->n < value)
-	{
-		prev = curr;
-		for (prev_index = 0; prev_index < step && curr->next; prev_index++)
-			curr = curr->next;
+        while (curr->index < size && curr->n < value)
+        {
+                prev = curr;
+                prev_index = curr->index;
+                jump_index = curr->index + step;
+                while (curr->index < jump_index && curr->next != NULL) {
+                        curr = curr->next;
+                }
+                printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
+                if(curr->index >= size -1 && curr->n < value)
+                        break;
+        }
 
+        printf("Value found between indexes [%lu] and [%lu]\n", prev_index, curr->index);
 
-		printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
-	}
+        while (prev != NULL && prev->index <= curr->index)
+        {
+                printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+                if (prev->n == value)
+                        return (prev);
+                prev = prev->next;
+        }
 
-	printf("Value found between indexes [%lu] and [%lu]\n", prev_index, curr->index);
-
-	while (prev && prev->index <= curr->index)
-	{
-		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
-		if (prev->n == value)
-			return (prev);
-		prev = prev->next;
-	}
-
-	return (NULL);
+        return (NULL);
 }
